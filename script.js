13fe49165d812687c6fe57a06caec1fe3fc1c10d
root <root@ip-172-31-31-230.ec2.internal>
2026-01-28 14:18:54 +0000

    script.js code added

---
+++ script.js
@@ -0,0 +1,5 @@
+function showMessage() {
+    document.getElementById("message").innerHTML = 
+        "Hello from JavaScript! Button clicked!";
+    console.log("Button clicked - message shown");
+}
